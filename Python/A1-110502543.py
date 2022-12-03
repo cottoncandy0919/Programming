ca1493a2ec2c4eeb80f0c1b9c966e732f2dc0307
cottoncandy0919 <aa4080217@yahoo.com.tw>
2022-12-03 16:26:36 +0800

    Create A1-110502543.py
    createA1

---
+++ Python/A1-110502543.py
@@ -0,0 +1,15 @@
+"""
+Assignment 1
+Name:歐子謙
+Student Number:110502543
+Course 2021-CE1003-B
+"""
+input1 = int(input())
+input2 = int(input())
+print(input1 + input2)
+print(input1 - input2)
+print(input1 * input2)
+print(input1 / input2)
+print(input1 ** input2)
+print(input1 // input2)
+print(input1 % input2)
